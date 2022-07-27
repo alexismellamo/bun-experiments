1645b6561360d8c8fbcc070d5b8fea2bababc1ab
Alexis Navarro <alexisnavarro23@gmail.com>
2022-07-27 18:59:08 -0500

    uwu

--- http.ts
+++ http.ts
@@ -1,8 +1,7 @@
-// http.js
+// http.ts
 export default {
   port: 3000,
   fetch(request: Request) {
-    console.log(request.url);
     return new Response("Welcome to Bun!");
   },
 };
--- write-file.ts
+++ write-file.ts
@@ -6,6 +6,3 @@ fs.writeFile("my-file.txt", "Hey there!", function (err) {
   }
   console.log("The file was saved!");
 });
-
-// Or
-fs.writeFileSync("/tmp/test-sync", "Hey there!");
